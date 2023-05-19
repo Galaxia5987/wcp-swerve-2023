4a94259a71bf2252905f2ee7933af55eb23a9bdc
Emma <emmalivne.03@gmail.com>
2023-05-19 11:13:47 +0300

    Add gripper default command

---
+++ src/main/java/frc/robot/subsystems/gripper/command/GripperDefaultCommand.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems.gripper.command;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.gripper.Gripper;
+
+public class GripperDefaultCommand extends CommandBase {
+    private final Gripper gripper = new Gripper();
+    private final double mainMotorPower;
+    private final double spinPower;
+    private final boolean intake;
+
+    public GripperDefaultCommand(double mainMotorPower, double spinPower, boolean intake) {
+        this.mainMotorPower = mainMotorPower;
+        this.spinPower = spinPower;
+        this.intake = intake;
+    }
+
+    @Override
+    public void execute() {
+        if (intake) {
+            gripper.setMainMotorPower(mainMotorPower);
+            gripper.setSpinPower(spinPower);
+        }
+        else {
+            gripper.setSpinPower(-spinPower);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        gripper.setSpinPower(0);
+        gripper.setMainMotorPower(0);
+    }
+}
