bd4d779a69a736ddf5c5357e1f8ff36476523b83
Emma <emmalivne.03@gmail.com>
2023-05-19 11:13:37 +0300

    Add constants

--- src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
@@ -1,4 +1,15 @@
 package frc.robot.subsystems.gripper;
 
 public class GripperConstants {
+    public static final double FALCON_TICKS_PER_ROTATION = 2048;
+    public static final double MAIN_MOTOR_GEARING = 0;
+    public static final double SPIN_MOTOR_LEFT_GEARING = 0;
+    public static final double SPIN_MOTOR_RIGHT_GEARING = 0;
+
+    public static final double VOLT_COMP_SATURATION = 12;
+    public static final int FALCON_TIMEOUT = 10; //[ms]
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+
 }
