683f50d0e6d851914e16bc5d69b74a1ff3eac080
Emma <emmalivne.03@gmail.com>
2023-05-19 11:45:31 +0300

    Add elevator constants

---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.elevator;
+
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+
+public class ElevatorConstants {
+    public static final TalonFXInvertType INVERT_TYPE = TalonFXInvertType.Clockwise;
+    public static final double VOLT_COMP_SATURATION = 12;
+    public static final int FALCON_TIMEOUT = 10;
+    public static final double DRUM_RADIUS = 0.03; // Radius of the elevator drum. [m]
+    public static final double TICKS_PER_METER = 2 * Math.PI * DRUM_RADIUS / 2048; // [tick]
+
+
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+
+}
