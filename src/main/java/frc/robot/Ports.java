98914d344efac94b97cc318d116131fed610d387
Emma <emmalivne.03@gmail.com>
2023-05-20 17:43:56 +0300

    Update ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -2,21 +2,21 @@ package frc.robot;
 
 public final class Ports {
 
-    public static final int ELE_MOTOR = 13;
+    public static final int ELE_MOTOR = 9;
 
-    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR_ID = 5;
-    public static final int FRONT_LEFT_MODULE_STEER_MOTOR_ID = 2;
-    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR_ID = 3;
-    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR_ID = 4;
-    public static final int REAR_LEFT_MODULE_DRIVE_MOTOR_ID = 7;
-    public static final int REAR_LEFT_MODULE_STEER_MOTOR_ID = 8;
-    public static final int REAR_RIGHT_MODULE_DRIVE_MOTOR_ID = 1;
-    public static final int REAR_RIGHT_MODULE_STEER_MOTOR_ID = 6;
+    public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR_ID = 1;
+    public static final int FRONT_LEFT_MODULE_STEER_MOTOR_ID = 6;
+    public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR_ID = 5;
+    public static final int FRONT_RIGHT_MODULE_STEER_MOTOR_ID = 2;
+    public static final int REAR_LEFT_MODULE_DRIVE_MOTOR_ID = 3;
+    public static final int REAR_LEFT_MODULE_STEER_MOTOR_ID = 4;
+    public static final int REAR_RIGHT_MODULE_DRIVE_MOTOR_ID = 7;
+    public static final int REAR_RIGHT_MODULE_STEER_MOTOR_ID = 8;
 
-    public static final int FRONT_LEFT_ENCODER_ID = 6;
-    public static final int FRONT_RIGHT_ENCODER_ID = 8;
-    public static final int REAR_LEFT_ENCODER_ID = 5;
-    public static final int REAR_RIGHT_ENCODER_ID = 7;
+    public static final int FRONT_LEFT_ENCODER_ID = 0;
+    public static final int FRONT_RIGHT_ENCODER_ID = 2;
+    public static final int REAR_LEFT_ENCODER_ID = 1;
+    public static final int REAR_RIGHT_ENCODER_ID = 3;
 
     public static boolean FRONT_LEFT_DRIVE_INVERTED = true;
     public static boolean FRONT_LEFT_ANGLE_INVERTED = true;
@@ -29,8 +29,8 @@ public final class Ports {
 
     public static class GripperPorts {
         public static int GRIPPER_MAIN_MOTOR = 0;
-        public static int GRIPPER_MAIN_SPIN_MOTOR = 0;
-        public static int GRIPPER_AUX_SPIN_MOTOR = 0;
+        public static int GRIPPER_MAIN_SPIN_MOTOR = 21;
+        public static int GRIPPER_AUX_SPIN_MOTOR = 22;
     }
 
     public static class UI {
