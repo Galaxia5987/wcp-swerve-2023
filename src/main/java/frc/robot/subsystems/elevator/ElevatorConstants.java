f18be4a35ceae1f5c92364e671cf3e635192be58
Emma <emmalivne.03@gmail.com>
2023-05-20 11:32:39 +0300

    Update elevator constants

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -6,7 +6,7 @@ public class ElevatorConstants {
     public static final TalonFXInvertType INVERT_TYPE = TalonFXInvertType.Clockwise; //TODO: check direction
     public static final double VOLT_COMP_SATURATION = 12;
     public static final int FALCON_TIMEOUT = 10;
-    public static final double DRUM_RADIUS = 0.03; // Radius of the elevator drum. [m] //TODO: check with mechanics team
+    public static final double DRUM_RADIUS = 0.03; // Radius of the elevator drum. [m]
     public static final double TICKS_PER_METER = 2 * Math.PI * DRUM_RADIUS / 2048; // [tick]
 
 
