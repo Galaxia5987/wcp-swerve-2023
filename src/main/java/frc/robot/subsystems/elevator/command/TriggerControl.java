da1a5da0bbbcdc2299e97f67e82fe96f2aa6ef66
Emma <emmalivne.03@gmail.com>
2023-05-20 22:13:26 +0300

    Add command to control the elevator with the triggers

---
+++ src/main/java/frc/robot/subsystems/elevator/command/TriggerControl.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems.elevator.command;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.ElevatorConstants;
+
+public class TriggerControl extends CommandBase {
+    private final Elevator elevator = Elevator.getInstance();
+
+    private final XboxController xbox;
+    private final Trigger rightTrigger;
+    private final Trigger leftTrigger;
+
+    public TriggerControl(XboxController xbox) {
+        this.xbox = xbox;
+        this.rightTrigger = new Trigger(()-> xbox.getRightTriggerAxis()>0.2);
+        this.leftTrigger = new Trigger(()-> xbox.getLeftTriggerAxis()>0.2);
+        addRequirements(elevator);
+    }
+
+    @Override
+    public void execute() {
+        elevator.setPower((xbox.getLeftTriggerAxis()* ElevatorConstants.DOWN_POWER_MULTIPLIER)-(xbox.getRightTriggerAxis()*ElevatorConstants.UP_POWER_MULTIPLIER));
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevator.setPower(0);
+    }
+}
