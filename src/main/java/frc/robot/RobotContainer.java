dca483434ee4c7fb3eac6337e0550194b1d35187
Emma <emmalivne.03@gmail.com>
2023-05-20 12:21:11 +0300

    Add robot controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,8 +5,14 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.drivetrain.commands.JoystickDrive;
+import frc.robot.subsystems.drivetrain.commands.XboxDrive;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.command.HoldInPlace;
+import frc.robot.subsystems.elevator.command.XboxControl;
+import frc.robot.subsystems.gripper.command.Intake;
 import frc.robot.subsystems.gyroscope.Gyroscope;
 
 public class RobotContainer {
@@ -14,12 +20,13 @@ public class RobotContainer {
 
     private final SwerveDrive swerveDrive = SwerveDrive.getInstance();
     private final Gyroscope gyroscope = Gyroscope.getInstance();
+    private final Elevator elevator = Elevator.getInstance();
 
     private final XboxController xboxController = new XboxController(0);
-    private final Joystick leftJoystick = new Joystick(1);
-    private final Joystick rightJoystick = new Joystick(2);
-    private final JoystickButton rb = new JoystickButton(xboxController, XboxController.Button.kRightBumper.value);
-    private final JoystickButton leftTrigger = new JoystickButton(leftJoystick, 1);
+    private final Trigger xboxRightTrigger = new Trigger(() -> xboxController.getRightTriggerAxis() > 0.2);
+    private final Trigger xboxLeftTrigger = new Trigger(() -> xboxController.getLeftTriggerAxis() > 0.2);
+    private final JoystickButton a = new JoystickButton(xboxController, XboxController.Button.kA.value);
+    private final JoystickButton b = new JoystickButton(xboxController, XboxController.Button.kB.value);
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -38,11 +45,17 @@ public class RobotContainer {
     }
 
     private void configureDefaultCommands() {
-        swerveDrive.setDefaultCommand(new JoystickDrive(leftJoystick, rightJoystick));
+        swerveDrive.setDefaultCommand(new XboxDrive(swerveDrive, xboxController));
+        elevator.setDefaultCommand(new HoldInPlace());
     }
 
     private void configureButtonBindings() {
-        leftTrigger.onTrue(new InstantCommand(gyroscope::resetYaw));
+        xboxLeftTrigger.onTrue(new InstantCommand(gyroscope::resetYaw));
+        xboxRightTrigger.whileTrue(
+                new InstantCommand(
+                        ()-> elevator.setPower(xboxController.getRightTriggerAxis())));
+        a.whileTrue(new Intake(0.1, 0.1, true));
+        b.whileTrue(new Intake(0.1, 0.1, false));
     }
 
 
