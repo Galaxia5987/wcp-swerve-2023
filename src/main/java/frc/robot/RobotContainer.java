7c137e12af061047bc27b925b21714d4c8a75fe9
Emma <emmalivne.03@gmail.com>
2023-05-20 17:45:07 +0300

    Add controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,17 +1,14 @@
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
-import frc.robot.subsystems.drivetrain.commands.JoystickDrive;
 import frc.robot.subsystems.drivetrain.commands.XboxDrive;
 import frc.robot.subsystems.elevator.Elevator;
-import frc.robot.subsystems.elevator.command.HoldInPlace;
-import frc.robot.subsystems.elevator.command.XboxControl;
+import frc.robot.subsystems.elevator.ElevatorConstants;
 import frc.robot.subsystems.gripper.command.Intake;
 import frc.robot.subsystems.gyroscope.Gyroscope;
 
@@ -27,6 +24,8 @@ public class RobotContainer {
     private final Trigger xboxLeftTrigger = new Trigger(() -> xboxController.getLeftTriggerAxis() > 0.2);
     private final JoystickButton a = new JoystickButton(xboxController, XboxController.Button.kA.value);
     private final JoystickButton b = new JoystickButton(xboxController, XboxController.Button.kB.value);
+    private final JoystickButton x = new JoystickButton(xboxController, XboxController.Button.kX.value);
+    private final JoystickButton lb = new JoystickButton(xboxController, XboxController.Button.kLeftBumper.value);
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -46,16 +45,19 @@ public class RobotContainer {
 
     private void configureDefaultCommands() {
         swerveDrive.setDefaultCommand(new XboxDrive(swerveDrive, xboxController));
-//        elevator.setDefaultCommand(new HoldInPlace());
     }
 
     private void configureButtonBindings() {
-        xboxLeftTrigger.onTrue(new InstantCommand(gyroscope::resetYaw));
-        xboxRightTrigger.whileTrue(
-                new InstantCommand(
-                        ()-> elevator.setPower(xboxController.getRightTriggerAxis())));
-        a.whileTrue(new Intake(0.1, 0.1, true));
-        b.whileTrue(new Intake(0.1, 0.1, false));
+        lb.onTrue(new InstantCommand(gyroscope::resetYaw));
+        xboxRightTrigger.whileTrue(new InstantCommand(()-> elevator.setPower(-xboxController.getRightTriggerAxis()* ElevatorConstants.POWER_MULTIPLIER)));
+        xboxRightTrigger.whileFalse(new InstantCommand(()-> elevator.setPower(0)));
+        xboxLeftTrigger.whileTrue(new InstantCommand(()-> elevator.setPower(xboxController.getLeftTriggerAxis()* ElevatorConstants.POWER_MULTIPLIER)));
+        xboxLeftTrigger.whileFalse(new InstantCommand(()-> elevator.setPower(0)));
+//        a.whileTrue(new Intake(0.1, 0.5, true));
+        a.toggleOnTrue(new Intake(0.1, 0.5, true));
+//        b.whileTrue(new Intake(0.1, 0.5, false));
+        b.toggleOnTrue(new Intake(0.1, 0.7, false));
+        x.onTrue(new InstantCommand(swerveDrive::printEncoderValues));
     }
 
 
