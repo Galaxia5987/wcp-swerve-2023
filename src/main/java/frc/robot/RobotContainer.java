272a25a8ddd06fb7d4ff0978c1dde6bcfa24fb80
GaiaZano05 <gaya.zano11@gmail.com>
2023-05-21 15:49:14 +0300

    Make 2 xbox controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,12 +3,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 import frc.robot.subsystems.drivetrain.commands.XboxDrive;
 import frc.robot.subsystems.elevator.Elevator;
-import frc.robot.subsystems.elevator.ElevatorConstants;
 import frc.robot.subsystems.elevator.command.TriggerControl;
 import frc.robot.subsystems.gripper.Gripper;
 import frc.robot.subsystems.gripper.command.Intake;
@@ -23,14 +23,17 @@ public class RobotContainer {
     private final Elevator elevator = Elevator.getInstance();
     private final Gripper gripper = Gripper.getInstance();
 
-    private final XboxController xboxController = new XboxController(0);
-    private final Trigger xboxRightTrigger = new Trigger(() -> xboxController.getRightTriggerAxis() > 0.2);
-    private final Trigger xboxLeftTrigger = new Trigger(() -> xboxController.getLeftTriggerAxis() > 0.2);
-    private final JoystickButton a = new JoystickButton(xboxController, XboxController.Button.kA.value);
-    private final JoystickButton b = new JoystickButton(xboxController, XboxController.Button.kB.value);
-    private final JoystickButton x = new JoystickButton(xboxController, XboxController.Button.kX.value);
-    private final JoystickButton lb = new JoystickButton(xboxController, XboxController.Button.kLeftBumper.value);
-    private final JoystickButton y = new JoystickButton(xboxController, XboxController.Button.kY.value);
+    private final XboxController xboxController1 = new XboxController(0);
+    private final XboxController xboxController2 = new XboxController(1);
+    private final Trigger xboxRightTrigger = new Trigger(() -> xboxController1.getRightTriggerAxis() > 0.2);
+    private final Trigger xboxLeftTrigger = new Trigger(() -> xboxController1.getLeftTriggerAxis() > 0.2);
+    private final JoystickButton a = new JoystickButton(xboxController1, XboxController.Button.kA.value);
+    private final JoystickButton b = new JoystickButton(xboxController1, XboxController.Button.kB.value);
+    private final JoystickButton x = new JoystickButton(xboxController1, XboxController.Button.kX.value);
+    private final JoystickButton lb2 = new JoystickButton(xboxController2, XboxController.Button.kLeftBumper.value);
+    private final JoystickButton lb = new JoystickButton(xboxController1, XboxController.Button.kLeftBumper.value);
+    private final JoystickButton rb = new JoystickButton(xboxController1, XboxController.Button.kRightBumper.value);
+    private final JoystickButton y = new JoystickButton(xboxController1, XboxController.Button.kY.value);
 
     /**
      * The container for the robot.  Contains subsystems, OI devices, and commands.
@@ -49,18 +52,18 @@ public class RobotContainer {
     }
 
     private void configureDefaultCommands() {
-        swerveDrive.setDefaultCommand(new XboxDrive(swerveDrive, xboxController));
-        elevator.setDefaultCommand(new TriggerControl(xboxController));
+        swerveDrive.setDefaultCommand(new XboxDrive(swerveDrive, xboxController2));
+        elevator.setDefaultCommand(new TriggerControl(xboxController1));
     }
 
     private void configureButtonBindings() {
-        lb.onTrue(new InstantCommand(gyroscope::resetYaw));
+        lb2.onTrue(new InstantCommand(gyroscope::resetYaw));
 
+        a.whileTrue(new Intake(0.4, 1191.0));
+        b.whileTrue(new Outtake(0.1, 0.4));
 
-        a.whileTrue(new Intake(0.6, 1191.0));
-        b.whileTrue(new Outtake(0.1, 0.9));
-        x.onTrue(new InstantCommand(()-> gripper.printValues(gripper.getPosition())));
         y.onTrue(new InstantCommand(gripper::resetEncoder));
+        x.onTrue(new InstantCommand(elevator::resetEncoder));
     }
 
 
