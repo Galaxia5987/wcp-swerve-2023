48f84cd2e3f0f14f7536759e7460f803dbcecff4
GaiaZano05 <gaya.zano11@gmail.com>
2023-08-06 19:04:12 +0300

    Add requirements and deadband

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "wcp-swerve-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "d3c0bb1fd8a6557a4ae3b1153e87e29633d0e64d";
-  public static final String GIT_DATE = "2023-08-01 10:56:27 EDT";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "fd53f4224b284627af84fb83ef6c637c4a3a2071";
+  public static final String GIT_DATE = "2023-08-06 12:04:12 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-08-06 11:26:13 EDT";
-  public static final long BUILD_UNIX_TIME = 1691335573845L;
+  public static final String BUILD_DATE = "2023-08-06 12:04:24 EDT";
+  public static final long BUILD_UNIX_TIME = 1691337864353L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/drivetrain/command/JoystickDrive.java
+++ src/main/java/frc/robot/subsystems/drivetrain/command/JoystickDrive.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.drivetrain.command;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Ports;
@@ -14,14 +15,15 @@ public class JoystickDrive extends CommandBase {
     public JoystickDrive(Joystick joystick1, Joystick joystick2){
         this.joystick1 = joystick1;
         this.joystick2 = joystick2;
+        addRequirements(swerveDrive);
     }
 
     @Override
     public void execute() {
         swerveDrive.drive(
-                -joystick1.getY(),
-                -joystick1.getX(),
-                -joystick2.getX(),
+                    MathUtil.applyDeadband(-joystick1.getY(), 0.1),
+                    MathUtil.applyDeadband(-joystick1.getX(), 0.1),
+                    MathUtil.applyDeadband(-joystick2.getX(), 0.1),
                 true
         );
     }
