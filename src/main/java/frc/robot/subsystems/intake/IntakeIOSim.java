8c08050d9fef318d6efac44f88ad8a5de21def17
Emma <emmalivne.03@gmail.com>
2023-08-15 17:19:47 +0300

    Add IntakeIOSim

--- src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -1,11 +1,23 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import frc.robot.utils.Utils;
+import frc.robot.utils.math.differential.Integral;
 
 public class IntakeIOSim implements IntakeIO{
     private final FlywheelSim angleMotor;
     private final FlywheelSim spinMotor;
+
+    private final Integral currentAngle = new Integral(0, 0);
+    private final PIDController angleFeedback;
+
+    private double angleMotorAppliedVoltage;
+    private double angleSetpoint;
+
+    private double spinMotorAppliedVoltage;
 
     public IntakeIOSim(){
         angleMotor = new FlywheelSim(
@@ -19,6 +31,8 @@ public class IntakeIOSim implements IntakeIO{
                 1,
                 IntakeConstants.SPIN_MOTOR_MOMENT_OF_INERTIA
         );
+
+        angleFeedback = new PIDController(0, 0, 0, 0.02);
     }
 
     @Override
@@ -26,6 +40,7 @@ public class IntakeIOSim implements IntakeIO{
         angleMotor.update(0.02);
         spinMotor.update(0.02);
 
+        currentAngle.update(angleMotor.getAngularVelocityRadPerSec());
 
         inputs.angle = currentAngle.get();
         inputs.angleSetpoint = angleSetpoint;
@@ -36,20 +51,19 @@ public class IntakeIOSim implements IntakeIO{
 
     @Override
     public double getAngle() {
+        return currentAngle.get();
     }
 
     @Override
     public void setAngle(double angle) {
-
-    }
-
-    @Override
-    public double getSpinMotorPower() {
-        return 0;
+        angleSetpoint = angle;
+        angleMotorAppliedVoltage = angleFeedback.calculate(MathUtil.angleModulus(currentAngle.get()), angle);
+        angleMotor.setInputVoltage(angleMotorAppliedVoltage);
     }
 
     @Override
     public void setSpinMotorPower(double power) {
-
+        spinMotorAppliedVoltage = power*IntakeConstants.VOLT_COMPENSATION_SATURATION;
+        spinMotor.setInputVoltage(spinMotorAppliedVoltage);
     }
 }
