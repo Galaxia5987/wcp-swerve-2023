25d489664d1b0b3f5c960c56508c52f0b1c72eba
GaiaZano05 <gaya.zano11@gmail.com>
2023-08-28 14:29:22 +0300

    build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "wcp-swerve-2023";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 18;
-  public static final String GIT_SHA = "e27246bf40ac208ffb22b1d35b3e5da3b11f4286";
-  public static final String GIT_DATE = "2023-08-27 07:35:47 EDT";
+  public static final int GIT_REVISION = 21;
+  public static final String GIT_SHA = "b088d2b8739281b0b3ea26865b60ed8ea8df5fc7";
+  public static final String GIT_DATE = "2023-08-27 11:46:11 EDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-08-27 11:30:16 EDT";
-  public static final long BUILD_UNIX_TIME = 1693150216183L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2023-08-28 07:27:35 EDT";
+  public static final long BUILD_UNIX_TIME = 1693222055555L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
